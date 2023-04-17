7b4873b2548c5468d6825cb920a34eeb2a7e09ff
Jeromin Rebatet <1513062+jeromin@users.noreply.github.com>
2023-04-17 03:41:42 +0000

    Adapting and improving orginal VFSBot script by @hnavidan
    - add undetected_chromedriver
    - add a second independent ajax check
    - split code into smaller functions
    - improved original appointment check to avoid false alarm when connexion is slow or website buggy
    - improved efficient by keeping browser instance open all along
    - add non headless mode via script option
    - Fix telegram commands (`/quit` and re-start and added `/check` on-demand
    - complete logging in terminal and in telegram
    
    Still not perfect 💪🏽

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import pytesseract
 import telegram
 from telegram.ext import Handler
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
 class WebError(Exception):
     pass
@@ -18,7 +18,8 @@ class AdminHandler(Handler):
         self.admin_ids = admin_ids
 
     def cb(self, update: telegram.Update, context):
-        update.message.reply_text('Unauthorized access!')
+        if not self.check_update(update):
+            update.message.reply_text('unauthorized access!')
 
     def check_update(self, update: telegram.update.Update):
         if update.message is None or update.message.from_user.id not in self.admin_ids:
@@ -27,14 +28,28 @@ class AdminHandler(Handler):
         return False
 
 def break_captcha():
-        img = cv2.imread('captcha.png')
-        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        se = cv2.getStructuringElement(cv2.MORPH_RECT, (8,8))
-        bg = cv2.morphologyEx(image, cv2.MORPH_DILATE, se)
-        out_gray = cv2.divide(image, bg, scale=255)
-        out_binary = cv2.threshold(out_gray, 0, 255, cv2.THRESH_OTSU )[1]
+    img = cv2.imread('captcha.png')
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    se = cv2.getStructuringElement(cv2.MORPH_RECT, (8,8))
+    bg = cv2.morphologyEx(image, cv2.MORPH_DILATE, se)
+    out_gray = cv2.divide(image, bg, scale=255)
+    out_binary = cv2.threshold(out_gray, 0, 255, cv2.THRESH_OTSU )[1]
+
+    captcha = pytesseract.image_to_string(out_binary, config='--psm 13 -c tessedit_char_whitelist=ABCDEFGHIJKLMNPQRSTUVWYZ')
+    denoised_captcha =  re.sub('[\W_]+', '', captcha).strip()
     
-        captcha = pytesseract.image_to_string(out_binary, config='--psm 13 -c tessedit_char_whitelist=ABCDEFGHIJKLMNPQRSTUVWYZ')
-        denoised_captcha =  re.sub('[\W_]+', '', captcha).strip()
-        
-        return denoised_captcha
+    return denoised_captcha
+
+class Message:
+    def __init__(self, update, context, channel_id):
+        self.update = update
+        self.context = context
+        self.channel_id = channel_id
+
+    def send(self, text):
+        print(text)
+        self.update.message.reply_text(text)
+
+    def broadcast(self, text):
+        self.send(text)
+        self.context.bot.send_message(chat_id=self.channel_id, text=text)
